13bf2eea3044920184c5e4dab75526758daae827
Arthur Del Esposte <arthurmde@gmail.com>
2016-10-17 10:28:56 -0200

    Add tests for incident model

--- spec/models/incident_spec.rb
+++ spec/models/incident_spec.rb
@@ -11,4 +11,47 @@ RSpec.describe Incident, type: :model do
     it { should belong_to(:student) }
     it { should belong_to(:user) }
   end
+
+  describe '#value_form' do
+    context 'without student' do
+      subject(:incident){ Incident.new }
+      it 'returns nil' do
+        expect(incident.value_form).to be_nil
+      end
+    end
+
+    context 'with student' do
+      subject(:incident){ FactoryGirl.create(:incident) }
+      it "returns student's id and name" do
+        expect(incident.value_form).to be_instance_of(String)
+      end
+    end
+  end
+
+  context 'date methods' do
+    subject(:incident){ FactoryGirl.create(:incident) }
+
+    before do
+      incident.date_incident = Date.new(2016, 8, 17)
+      incident.save!
+    end
+
+    describe '#date_insident_format' do
+      it 'respond with day/month/year format' do
+        expect(incident.date_insident_format).to eq('17/08/2016')
+      end
+    end
+
+    describe '#sdate' do
+      it 'respond with day/month/year format' do
+        expect(incident.sdate).to eq('17/08/2016')
+      end
+    end
+
+    describe '#fdate' do
+      it 'respond with day/month/year format' do
+        expect(incident.fdate).to eq('17/08/2016')
+      end
+    end
+  end
 end

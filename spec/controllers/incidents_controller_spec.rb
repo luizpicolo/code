cfc3b9c82eb6869e8157502c799a4bf1d10bdf21
Bruno Oliveira <brunoao86@gmail.com>
2016-10-24 16:55:58 -0200

    Add alerts for incidents

--- spec/controllers/incidents_controller_spec.rb
+++ spec/controllers/incidents_controller_spec.rb
@@ -72,25 +72,49 @@ RSpec.describe IncidentsController, type: :controller do
         end
       end
 
-      describe 'GET #update' do
+      describe 'PUT #update' do
         let!(:incident) { FactoryGirl.create :incident, user: user }
         let(:valid_params) { { description: 'A new description' } }
+        let(:invalid_params) { { description: '' } }
 
-        it 'accepts changes' do
-          process :update, method: :put, params: { id: incident.id, incident: valid_params}
-          expect(response).to redirect_to incident
+        context 'with valid params' do
+          it 'accepts changes' do
+            process :update, method: :put, params: { id: incident.id, incident: valid_params}
+            expect(flash[:success]).to eql('Ocorrência atualizada com sucesso')
+            expect(response).to redirect_to incident_path(incident)
+          end
+        end
+
+        context 'with invalid params' do
+          it 'renders the edit template' do
+            put :update, id: incident.id, incident: invalid_params
+            expect(response).to render_template(:edit)
+          end
+
+          it 'adds error to flash[:error]' do
+            put :update, id: incident.id, incident: invalid_params
+
+            incident = assigns(:incident)
+
+            error_msg = []
+            incident.errors.full_messages.each do |msg|
+              error_msg << "#{msg}"
+            end
+
+            expect(flash[:error]).to eq(error_msg)
+          end
         end
       end
 
-      describe 'GET #create' do
+      describe 'POST #create' do
         context "with valid attributes" do
           it 'create a new incident' do
             expect{
               process :create, method: :post, params: { incident: {student_id: FactoryGirl.create(:student).id, user_id: user.id, date_incident: Time.zone.now, description: Faker::Lorem.paragraph , soluction: Faker::Lorem.paragraph }}
-              }.to change(Incident,:count).by(1)
+            }.to change(Incident,:count).by(1)
           end
 
-          it "redirects to the new contact" do
+          it "redirects to the new incident" do
             process :create, method: :post, params: { incident: {student_id: FactoryGirl.create(:student).id, user_id: user.id, date_incident: Time.zone.now, description: Faker::Lorem.paragraph , soluction: Faker::Lorem.paragraph }}
             expect(response).to redirect_to Incident.last
           end
@@ -103,30 +127,59 @@ RSpec.describe IncidentsController, type: :controller do
             }.to_not change(Incident,:count)
           end
 
-          it "re-renders the new method" do
+          it "renders the new template" do
             process :create, method: :post, params: { incident: {student_id: FactoryGirl.create(:student).id, user_id: user.id, date_incident: nil, description: nil , soluction: nil }}
             expect(response).to render_template(:new)
+          end
+
+          it "adds error to flash[:error]" do
+            process :create, method: :post, params: { incident: {student_id: FactoryGirl.create(:student).id, user_id: user.id, date_incident: nil, description: nil , soluction: nil }}
+
+            incident = assigns(:incident)
+
+            error_msg = []
+            incident.errors.full_messages.each do |msg|
+              error_msg << "#{msg}"
+            end
+
+            expect(flash[:error]).to eq(error_msg)
           end
         end
       end
 
       describe 'DELETE destroy' do
-        before :each do
-          @incident = FactoryGirl.create(:incident, user: user)
+        let!(:incident) { FactoryGirl.create :incident }
+
+        context 'successful destroy' do
+          it "deletes the incident" do
+            expect{
+              process :destroy, method: :delete, params: { id: incident}
+            }.to change(Incident,:count).by(-1)
+          end
+
+          it "redirects to index" do
+            process :destroy, method: :delete, params: { id: incident}
+            expect(response).to redirect_to incidents_path
+            expect(flash[:success]).to eql('Ocorrência excluída com sucesso')
+          end
         end
 
-        it "deletes the incident" do
-          expect{
-            process :destroy, method: :delete, params: { id: @incident}
-          }.to change(Incident,:count).by(-1)
-        end
+        context 'unsuccessful destroy' do
+          it "adds error to flash[:error]" do
+            allow_any_instance_of(Incident).to receive(:destroy).and_return(false)
 
-        it "redirects to contacts#index" do
-          process :destroy, method: :delete, params: { id: @incident}
-          expect(response).to redirect_to '/incidents'
+            delete :destroy, { id: incident }
+            expect(flash[:error]).to eq('Erro ao excluir ocorrência')
+          end
+
+          it "redirects to index" do
+            allow_any_instance_of(Incident).to receive(:destroy).and_return(false)
+
+            delete :destroy, { id: incident }
+            expect(response).to redirect_to incidents_path
+          end
         end
       end
     end
   end
 end
-

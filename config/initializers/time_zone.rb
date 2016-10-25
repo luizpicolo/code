9927ea66becbdb3a00615c9ed9a2fac327fef372
David BF <burretfresket@gmail.com>
2016-10-25 23:01:40 +0200

    Add I18n fallbacks pt & en.
    * Move kaminari translations from pt-BR to pt

--- config/initializers/time_zone.rb
+++ config/initializers/time_zone.rb
@@ -1,5 +1,7 @@
 Rails.env.on(:any) do
   # Configuration default for time zone
   config.time_zone = 'Brasilia'
-  config.i18n.default_locale = :'pt-BR'
+  config.i18n.available_locales = ['pt-BR', :pt,:en]
+  config.i18n.default_locale = 'pt-BR'
+  config.i18n.fallbacks = ['pt-BR', :pt, :en]
 end
